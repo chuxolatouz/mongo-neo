3414d0210f7df691765c259c61eb6953e035fe97
Jesus Rincon <00jjessejj00@gmail.com>
2016-03-07 22:13:45 -0430

    Conectado a neoj4 y a mongodb, falta guardar datos en arreglos para insertar en neo4j

--- martell.js
+++ martell.js
@@ -1,51 +1,39 @@
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
-var ObjectId = require('mongodb').ObjectID;
+var request = require("request");
 var url = 'mongodb://localhost:27017/proyectofase1';
+var r=require("request");
+var txUrl = "http://localhost:7474/db/data/transaction/commit";
 
-var insertDocument = function(db, callback) {
-   db.collection('restaurants').insertOne( {
-      "address" : {
-         "street" : "2 Avenue",
-         "zipcode" : "10075",
-         "building" : "1480",
-         "coord" : [ -73.9557413, 40.7720266 ]
-      },
-      "borough" : "Manhattan",
-      "cuisine" : "Italian",
-      "grades" : [
-         {
-            "date" : new Date("2014-10-01T00:00:00Z"),
-            "grade" : "A",
-            "score" : 11
-         },
-         {
-            "date" : new Date("2014-01-16T00:00:00Z"),
-            "grade" : "B",
-            "score" : 17
-         }
-      ],
-      "name" : "Vella",
-      "restaurant_id" : "41704620"
-   }, function(err, result) {
-    assert.equal(err, null);
-    console.log("Inserted a document into the restaurants collection.");
-    callback();
-  });
-};
+var personajes = [];
+var casas = new Array;
+
+function cypher(query,params,cb) {
+  r.post({uri:txUrl,
+          json:{statements:[{statement:query,parameters:params}]}},
+         function(err,res) { cb(err,res.body)})
+}
+
+var query="MATCH (n) RETURN n LIMIT 25"
+var params={limit: 10}
+var cb=function(err,data) { console.log(JSON.stringify(data)) }
+
+cypher(query,params,cb)
 
 var findPersonajes = function(db, callback) {
    var cursor =db.collection('personajes').find();
    cursor.each(function(err, doc) {
       assert.equal(err, null);
       if (doc != null) {
-         console.dir(doc);
+         //console.dir(doc);
       } else {
          callback();
       }
    });
 };
+
 var findCasas = function(db, callback) {
+
    var cursor =db.collection('casas').find();
    cursor.each(function(err, doc) {
       assert.equal(err, null);
@@ -62,7 +50,9 @@ MongoClient.connect(url, function(err, db) {
   findPersonajes(db, function() {
 
   });
-  findCasas(db, function() {
-      db.close();
+   findCasas(db, function() {
+
+        db.close();
   });
+
 });
